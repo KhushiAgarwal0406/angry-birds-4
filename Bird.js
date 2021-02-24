ceac3211f99c27f609c113471820d7a55f3c9f91
DeepthiTchr <70970105+DeepthiTchr@users.noreply.github.com>
2021-02-24 20:05:23 +0530

    Update Bird.js

--- Bird.js
+++ Bird.js
@@ -3,23 +3,27 @@ class Bird extends BaseClass {
     super(x,y,50,50);
     this.image = loadImage("sprites/bird.png");
     this.smokeImage = loadImage("sprites/smoke.png");
-    this.trajectory = []
+    this.trajectory =[];
   }
 
   display() {
     //this.body.position.x = mouseX;
     //this.body.position.y = mouseY;
+
     super.display();
+console.log(this.body.position.x)
+console.log(this.body.velocity.x)
 
-    if(this.body.velocity.x>1 && this.body.position.x>200){
 
-    var position = [this.body.position.x,this.body.position.y];
-    this.trajectory.push(position)
-    
+
+   if(this.body.velocity.x>1 && this.body.position.x > 200){
+      var position = [this.body.position.x, this.body.position.y];
+      this.trajectory.push(position);
     }
+   
 
-    for(var i = 0; i<this.trajectory.length; i++){
-    image(this.smokeImage,this.trajectory[i][0],this.trajectory[i][1])
+    for(var i=0; i<this.trajectory.length; i++){
+      image(this.smokeImage, this.trajectory[i][0], this.trajectory[i][1]);
     }
   }
 }
